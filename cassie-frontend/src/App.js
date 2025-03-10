8165f71bc8678fdaa6480ce27271f3f9a8803d87
Maddie Mortensen <maddiemortensen@Maddies-MacBook-Air.local>
2025-03-09 22:48:41 -0700

    Added transitions

--- cassie-frontend/src/App.js
+++ cassie-frontend/src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { TransitionGroup } from 'react-transition-group';
 import { AuthProvider } from './context/AuthContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,44 +12,53 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import ProtectedRoute from './components/ProtectedRoute';
 import CreateEvent from './pages/CreateEvent';
+import PageTransition from './components/PageTransition';
 
 import './App.css'; // Keep global styles
 
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
+const AppContent = () => {
+    const location = useLocation();
+
+    return (
         <div className="app-container">
-          <Header />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/events" element={<EventsList />} />
-              <Route 
-                path="/dashboard" 
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route 
-                path="/events/create" 
-                element={
-                  <ProtectedRoute adminOnly>
-                    <CreateEvent />
-                  </ProtectedRoute>
-                } 
-              />
-            </Routes>
-          </div>
-          <Footer />
+            <Header />
+            <PageTransition location={location}>
+                <Routes location={location}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/events" element={<EventsList />} />
+                    <Route 
+                        path="/dashboard" 
+                        element={
+                            <ProtectedRoute>
+                                <Dashboard />
+                            </ProtectedRoute>
+                        } 
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route 
+                        path="/events/create" 
+                        element={
+                            <ProtectedRoute adminOnly>
+                                <CreateEvent />
+                            </ProtectedRoute>
+                        } 
+                    />
+                </Routes>
+            </PageTransition>
+            <Footer />
         </div>
-      </Router>
-    </AuthProvider>
-  );
+    );
+};
+
+function App() {
+    return (
+        <AuthProvider>
+            <Router>
+                <AppContent />
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
